6f847704750d05057c29d02b824ac78c049fa6a1
Alessandro Barros <ale_alessandro_ale@hotmail.com>
2020-10-14 14:22:11 -0300

    add(user): model spec

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe "validations" do
+    let!(:user) { create(:user) }
+
+    it { is_expected.to validate_uniqueness_of(:email).ignoring_case_sensitivity }
+    
+    it { is_expected.to validate_presence_of(:email)  }
+    it { is_expected.to validate_presence_of(:name)   }
+  end
+end
