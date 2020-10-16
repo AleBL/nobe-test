81ada1fe30eee200f8321ec1dfb650840f370a03
Alessandro Barros <ale_alessandro_ale@hotmail.com>
2020-10-15 21:35:05 -0300

    add: account and account number created when user was created

--- app/models/account.rb
+++ app/models/account.rb
@@ -5,4 +5,12 @@ class Account < ApplicationRecord
   validates :number,  presence: true
 
   validates_numericality_of :balance, greater_than_or_equal_to: 0
+
+  before_validation :generate_number
+
+  private
+
+  def generate_number
+    self.number = "%05d" % self.user.id if self.user
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,15 @@ class User < ApplicationRecord
   validates_uniqueness_of :email
 
   has_one :account, dependent: :destroy
+
+  after_create :create_account
+
+  private
+
+  def create_account
+    @ac = Account.new(user: self, balance: 0)
+    unless @ac.save
+      raise "Account could not be created"
+    end
+  end
 end
